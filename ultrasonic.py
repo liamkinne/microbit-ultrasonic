233068af0781c19f6b78685f492567a2b3f08d1b
liamkinne <kinneliam@gmail.com>
2018-01-16 18:29:45 +1100

    added get function

--- ultrasonic.py
+++ ultrasonic.py
@@ -5,6 +5,8 @@ class Ultrasonic:
 		self.pin_trig = pin_trig
 		self.pin_echo = pin_echo
 
+		self.pin_echo.read_digital() # set as input
+
 		self.units = {
 		  'mm' : 58 * 10,
 		  'cm' : 58,
@@ -18,3 +20,18 @@ class Ultrasonic:
 	def set_units(self, unit):
 		if unit in self.units.keys():
 			self.unit = unit
+
+	def get():
+		# pulse trig pin for >10uS
+		self.pin_trig.write_digital(True)
+		sleep(1)
+		self.pin_trig.write_digital(False)
+		
+		# read pulse length of echo pin
+		while self.pin_echo.read_digital() == False:
+			pass
+		time_start = running_time()
+		while self.pin_echo.read_digital() == True:
+			pass
+		time_end = running_time()
+		return (time_end - time_start) * self.unit
